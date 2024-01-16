b51a983e713ffa15d0bb2946d6d51386e6074ece
smithnj13 <SmithNJ13@hotmail.com>
2024-01-16 04:29:00 +0000

    playing around w/ shapes

--- frontend/src/components/TeamBanner/index.jsx
+++ frontend/src/components/TeamBanner/index.jsx
@@ -1,17 +1,22 @@
 import "./style.css"
+import { Triangle } from "react-shapes"
 
-const TeamBanner = ({icon, name}) => {
+const TeamBanner = ({ icon, name }) => {
   return (
-    <div id="panel">
-      <img id="teamIcon" src={icon}></img>
-      <h1 id="teamName">{name}</h1>
-      <p>xG:</p>
-      <div id="inputArea">
-        <input id="xG" type="value" placeholder="null"></input>
-        <button id="button">✔️</button>
+    <div id="teamBannerWrapper">
+      <div id="panel">
+        <img id="teamIcon" src={icon} alt="Team Icon"></img>
+        <h1 id="teamName">{name}</h1>
+        <p>xG:</p>
+        <div id="inputArea">
+          <input id="xG" type="value" placeholder="null"></input>
+          <button id="button">✔️</button>
+        </div>
       </div>
+      <Triangle width={300} height={300} fill={{color: "green"}} transform={{transform: "rotate(180deg)"}}/>
     </div>
-  )
-}
+  );
+};
+
 
 export default TeamBanner
