a25c2a286cf6dcbedad2b9adb56bdd8d93685e56
SmithNJ13 <smithNJ13@hotmail.com>
2024-12-30 16:31:29 +0000

    added flipping animation and submission form to each MatchCard

--- frontend/src/components/TeamBanner/index.jsx
+++ frontend/src/components/TeamBanner/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
 import "./style.css";
 import MatchCard from "../../assets/matchcard.svg?react";
 
@@ -6,6 +7,7 @@ const TeamBanner = ({id, matchID, teamIcon, teamName, teamColour, side, buttonCl
   const [clicked, setClicked] = useState(false)
   const [pxG, setpxG] = useState(0.0)
   const [size, setSize] = useState({fontSize: 34})
+  const [flipped, setFlipped] = useState(false)
 
   const sizeHandler = (teamName) => {
       if(teamName.length > 15) {
@@ -18,19 +20,15 @@ const TeamBanner = ({id, matchID, teamIcon, teamName, teamColour, side, buttonCl
       }
   }
 
-  // const handleClick = () => {
-  //   const pxGDouble = parseFloat(pxG)
-  //   const Info = {
-  //     matchID: matchID,
-  //     teamName: teamName,
-  //     side: side,
-  //     pxG: pxGDouble,
-  //     inactive: true
-  //   }
-  //   console.log(Info)
-  //   setClicked(true)
-  //   buttonClick(Info)
-  // }
+const handleClick = () => {
+  const card = document.querySelector(`#matchcard${id}`)
+  console.log(card)
+  setFlipped(true)
+  const flair2 = card.querySelector('#flair2');
+    if (flair2) {
+      flair2.style.display = "none";
+    }
+}
 
   useEffect(() => {
     const cardId = `#matchcard${id}`
@@ -52,16 +50,42 @@ const TeamBanner = ({id, matchID, teamIcon, teamName, teamColour, side, buttonCl
   
   return (
     <>
-    <div id={`matchcard${id}`} className="relative w-[300px] h-[440px]">
-      <MatchCard className="cardBody"/>
-      <div id="top">
-        <img id="teamIcon" className="absolute top-[11%] left-[50%] w-[120px] h-[120px]" src={teamIcon} alt="Team_Icon"></img>
-        <h1 id="teamName" className="absolute align-center w-[220px] text-white top-[55%] left-[50%]" style={size}>{teamName}</h1>
-      </div>
-      <div id="bottom" className="absolute bottom-[20%] left-[50%] p-[1px] align-center text-white">
-        <button className="relative">Place Predictions</button>
-      </div>
-    </div>
+    <motion.div id={`matchcard${id}`} className="relative w-[300px] h-[440px]" 
+    animate={{
+      rotateY: flipped ? 180 : 0,
+      scale: flipped ? [1, 1.1, 1] : 1
+    }}
+    transition={{
+      duration: 1.5, 
+      ease: "easeInOut",
+      scale: {duration: 1.5}
+      }}>
+        {!flipped ? (
+          <div id="front">
+            <MatchCard className="cardBody"/>
+            <div id="top">
+              <img id="teamIcon" className="absolute top-[11%] left-[50%] w-[120px] h-[120px]" src={teamIcon} alt="Team_Icon"></img>
+              <h1 id="teamName" className="absolute align-center w-[220px] text-white top-[55%] left-[50%]" style={size}>{teamName}</h1>
+            </div>
+            <div id="bottom" className="absolute bottom-[20%] left-[50%] p-[1px] align-center text-white">
+              <button className="relative" onClick={handleClick}>Place Predictions</button>
+            </div>
+          </div>
+        ) : (
+          <div id="back">
+            <MatchCard className="cardBody"/>
+            <div id="predictionSheet" className="absolute top-[10%] left-[20%] align-center justify-center text-white">
+              <form className="flex flex-col gap-[20px]">
+                <p className="flex flex-col">xG: <input className="text-black"></input></p>
+                <p className="flex flex-col">First Player to Score: <input className="text-black"></input></p>
+                <p className="flex flex-col">AnotherPredictionStat: <input className="text-black"></input></p>
+                <p className="flex flex-col">Clean Sheet? <input className="text-black"></input></p>
+                <button className="ml-[30%] border-white border-[1px] rounded w-[60px] hover:text-green-500">Submit</button>
+              </form>
+            </div>
+          </div>
+        )}
+    </motion.div>
     </>
   );
 };
