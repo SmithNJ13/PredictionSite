857992f6993b66a360b4c9732bf7cb5b03e3322b
SmithNJ13 <smithNJ13@hotmail.com>
2025-08-23 19:04:54 +0100

    updating HTML & port

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,6 @@ require("dotenv").config()
 const app = require("./app")
 
 
-app.listen(process.env.PORT, "127.0.0.1", () => {
+app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}`)
 })
